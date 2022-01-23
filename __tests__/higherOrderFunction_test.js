eff5fa23b7309ad3e4ea8fd27347239794539904
Andrew Youngho Chaa <andrew.yh.chaa@gmail.com>
2022-01-23 12:12:56 +0000

    fix: use strict equal

--- __tests__/higherOrderFunction_test.js
+++ __tests__/higherOrderFunction_test.js
@@ -4,5 +4,22 @@ const selector  = (country, school) =>
 const findStudentsBy = (friends, selector) =>  friends.filter(selector)
 
 test('findStudentsBy', () => {
-  expect(1).toBe(1)
+
+  const curry = {
+    address : {
+      country: 'UK'
+    },
+    school : 'LSE'
+  }
+  const church = {
+    address : {
+      country: 'US'
+    },
+    school : 'Princeton'
+  }
+
+  expect(findStudentsBy(
+    [curry, church],
+    selector('US', 'Princeton')
+    )).toStrictEqual([church])
 })
