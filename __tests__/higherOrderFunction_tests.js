c8ff2f6d71fc356f6d7a5a9b87848e15c26bb7d1
Andrew Youngho Chaa <andrew.yh.chaa@gmail.com>
2022-01-23 13:57:04 +0000

    multiplier

---
+++ __tests__/higherOrderFunction_tests.js
@@ -0,0 +1,19 @@
+test('pass selector to findStudentsBy', () => {
+
+  const selector  = (country, school) =>
+    (student) => student.address.country === country && student.school === school
+
+  const findStudentsBy = (friends, selector) =>  friends.filter(selector)
+  const curry =  { address : { country: 'UK' }, school : 'LSE' }
+  const church = { address : { country: 'US' }, school : 'Princeton' }
+
+  expect(findStudentsBy([curry, church], selector('US', 'Princeton')))
+    .toStrictEqual([church])
+})
+
+test('pass multiplier as operation', () => {
+  const applyOperation = (a, b, opt) => opt(a,b)
+  const multiplier = (a, b) => a * b
+
+  expect(applyOperation(2, 3, multiplier)).toBe(6)
+})
